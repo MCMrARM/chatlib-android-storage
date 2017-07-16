57750d89d58194869faad1fc5f69b54684de1d2f
MCMrARM <mcmrarm@gmail.com>
2017-07-16 21:38:58 +0200

    Save MODE messages

--- storage/src/main/java/io/mrarm/chatlib/android/storage/MessageStorageHelper.java
+++ storage/src/main/java/io/mrarm/chatlib/android/storage/MessageStorageHelper.java
@@ -1,12 +1,17 @@
 package io.mrarm.chatlib.android.storage;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import io.mrarm.chatlib.dto.ChannelModeMessageInfo;
 import io.mrarm.chatlib.dto.MessageInfo;
 import io.mrarm.chatlib.dto.MessageSenderInfo;
 import io.mrarm.chatlib.dto.NickChangeMessageInfo;
@@ -16,6 +21,7 @@ class MessageStorageHelper {
 
     private static final String PROP_BATCH = "batch";
     private static final String PROP_NICKCHANGE_NEWNICK = "newNick";
+    private static final String PROP_CHANNELMODE_ENTRIES = "entries";
 
 
     private static final Gson gson = new Gson();
@@ -30,7 +36,16 @@ class MessageStorageHelper {
         JsonObject o = gson.fromJson(extraData, JsonObject.class);
         if (type == MessageInfo.MessageType.NICK_CHANGE)
             return new NickChangeMessageInfo(sender, date, o.get(PROP_NICKCHANGE_NEWNICK).getAsString());
-        MessageInfo.Builder builder = new MessageInfo.Builder(sender, text, type);
+        MessageInfo.Builder builder;
+        if (type == MessageInfo.MessageType.MODE) {
+            JsonArray entriesArray = o.get(PROP_CHANNELMODE_ENTRIES).getAsJsonArray();
+            List<ChannelModeMessageInfo.Entry> entries = new ArrayList<>(entriesArray.size());
+            for (JsonElement e : entriesArray)
+                entries.add(gson.fromJson(e.getAsJsonObject(), ChannelModeMessageInfo.Entry.class));
+            builder = new ChannelModeMessageInfo.Builder(sender, entries);
+        } else {
+            builder = new MessageInfo.Builder(sender, text, type);
+        }
         builder.setDate(date);
         if (o.has(PROP_BATCH)) {
             // TODO: find the batch
@@ -60,6 +75,10 @@ class MessageStorageHelper {
         if (info instanceof NickChangeMessageInfo) {
             NickChangeMessageInfo nickChangeMessage = ((NickChangeMessageInfo) info);
             object.addProperty(PROP_NICKCHANGE_NEWNICK, nickChangeMessage.getNewNick());
+        }
+        if (info instanceof ChannelModeMessageInfo) {
+            ChannelModeMessageInfo modeMessage = ((ChannelModeMessageInfo) info);
+            object.add(PROP_CHANNELMODE_ENTRIES, gson.toJsonTree(modeMessage.getEntries()));
         }
         return gson.toJson(object);
     }
