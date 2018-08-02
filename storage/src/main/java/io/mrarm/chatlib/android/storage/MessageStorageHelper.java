ea8ea82a1559392ab64726c921a7107ab061f1bd
MCMrARM <mcmrarm@gmail.com>
2018-08-02 19:25:00 +0200

    Properly null user and host in MessageSenderInfo

--- storage/src/main/java/io/mrarm/chatlib/android/storage/MessageStorageHelper.java
+++ storage/src/main/java/io/mrarm/chatlib/android/storage/MessageStorageHelper.java
@@ -67,19 +67,29 @@ class MessageStorageHelper {
     }
 
     static String serializeSenderInfo(MessageSenderInfo sender) {
-        return (sender.getNickPrefixes() == null ? "" : sender.getNickPrefixes().toString()) + " " + sender.getNick() + "!" + sender.getUser() + "@" + sender.getHost();
+        return (sender.getNickPrefixes() == null ? "" :
+                sender.getNickPrefixes().toString()) + " " + sender.getNick() +
+                (sender.getUser() != null ? "!" + sender.getUser() : "") +
+                (sender.getHost() != null ? "@" + sender.getHost() : "");
     }
 
     static MessageSenderInfo deserializeSenderInfo(String serialized, UUID uuid) {
         if (serialized.equals(""))
             return null;
-        int iof = serialized.indexOf(' ');
-        String prefixes = serialized.substring(0, iof);
-        int iof2 = serialized.indexOf('!', iof + 1);
-        String nick = serialized.substring(iof + 1, iof2);
-        iof = serialized.indexOf('@', iof2 + 1);
-        String user = serialized.substring(iof2 + 1, iof);
-        String host = serialized.substring(iof + 1);
+        int piof = serialized.indexOf(' ');
+        String prefixes = serialized.substring(0, piof);
+
+        String nick, user = null, host = null;
+        int iof = serialized.indexOf('!', piof);
+        int iof2 = serialized.indexOf('@', (iof == -1 ? piof + 1 : iof + 1));
+        if (iof != -1 || iof2 != -1)
+            nick = serialized.substring(piof + 1, (iof != -1 ? iof : iof2));
+        else
+            nick = serialized.substring(piof + 1);
+        if (iof != -1)
+            user = serialized.substring(iof + 1, (iof2 == -1 ? serialized.length() : iof2));
+        if (iof2 != -1)
+            host = serialized.substring(iof2 + 1);
         return new MessageSenderInfo(nick, user, host, prefixes.length() > 0 ? new NickPrefixList(prefixes) : null, uuid);
     }
 
